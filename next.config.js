0529c0e3a369d2dfaf7e363ea9526c057c2e8f86
Oleg Smetanin <oleg@smetan.in>
2018-11-11 14:06:37 +0300

    add polyfills

--- next.config.js
+++ next.config.js
@@ -1,2 +1,22 @@
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const withTypescript  = require('@zeit/next-typescript');
+
+// https://github.com/zeit/next.js/issues/4636
+module.exports = withTypescript({
+    webpack: function(config, {dev}) {
+        if(!dev) {
+            const originalEntry = config.entry;
+
+            config.entry = async() => {
+                const entries = await originalEntry();
+
+                if(entries['main.js'] && !entries['main.js'].includes('./src/common/polyfills.js')) {
+                    entries['main.js'].unshift('./src/common/polyfills.js');
+                }
+
+                return entries;
+            };
+        }
+
+        return config;
+    },
+})
