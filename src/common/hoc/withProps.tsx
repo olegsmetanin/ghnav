1e57b342f00de516b7cfa2be5b6b462394ad6dbb
Oleg Smetanin <oleg@smetan.in>
2018-11-17 19:31:30 +0300

    add withProps HOC for history

---
+++ src/common/hoc/withProps.tsx
@@ -0,0 +1,8 @@
+
+export const withProps = <P extends object>(propsfab: () => P) => (Component:  React.ComponentType<P>) =>
+  class WithProps extends React.Component<P> {
+    public render() {
+      const props = { ...this.props as any as object, ...propsfab() }
+      return <Component {...props} />
+    }
+  }
